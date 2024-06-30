218df551c184337e6ef89988b87febf311deded5
Rafael Rodrigues <rafaelrodrigues2903@gmail.com>
2024-06-30 17:45:07 -0300

    refactor: extract postgres typeorm config to class

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,30 +2,20 @@ import { ClassSerializerInterceptor, Module } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
-import { join } from 'path';
+import { APP_INTERCEPTOR } from '@nestjs/core';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
+import { PostgresConfigService } from './config/postgres.config.service';
 import { MoviesModule } from './movies/movies.module';
 import { UsersModule } from './users/users.module';
-import { APP_INTERCEPTOR } from '@nestjs/core';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
     TypeOrmModule.forRootAsync({
       inject: [ConfigService],
-      useFactory: (configService: ConfigService) => ({
-        type: 'postgres',
-        host: configService.get('PGHOST'),
-        port: +configService.get('PGPORT'),
-        username: configService.get('PGUSER'),
-        password: configService.get('PGPASSWORD'),
-        database: configService.get('PGDATABASE'),
-        entities: [join(__dirname, '**', '*.entity.{ts,js}')],
-        synchronize: true,
-        autoLoadEntities: true,
-      }),
+      useClass: PostgresConfigService,
     }),
     MoviesModule,
     UsersModule,
---
+++ src/config/postgres.config.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { TypeOrmModuleOptions, TypeOrmOptionsFactory } from '@nestjs/typeorm';
+import { join } from 'path';
+
+@Injectable()
+export class PostgresConfigService implements TypeOrmOptionsFactory {
+  constructor(private configService: ConfigService) {}
+  createTypeOrmOptions(): TypeOrmModuleOptions {
+    return {
+      type: 'postgres',
+      host: this.configService.get('PGHOST'),
+      port: +this.configService.get('PGPORT'),
+      username: this.configService.get('PGUSER'),
+      password: this.configService.get('PGPASSWORD'),
+      database: this.configService.get('PGDATABASE'),
+      entities: [join(__dirname, '**', '*.entity.{ts,js}')],
+      synchronize: true,
+      autoLoadEntities: true,
+    };
+  }
+}
